7dabeeb1ca9b38c26818116bca5052504c4ee0a3
Fineas <r0845664@student.thomasmore.be>
2023-09-10 01:48:28 +0200

    project solution in visual studio aangemaakt

---
+++ encryption_decryption_tool/encryption_decryption_tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace encryption_decryption_tool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
