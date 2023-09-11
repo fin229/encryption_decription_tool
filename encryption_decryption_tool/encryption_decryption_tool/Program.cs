c89d028a350c42d3365ce68b4dc723a74280b983
Fineas <r0845664@student.thomasmore.be>
2023-09-11 13:28:45 +0200

    functies voor de ceasar cipher afgewerkt en logica geprogrammeerd om een gegeven bericht te encrypteren

--- encryption_decryption_tool/encryption_decryption_tool/Program.cs
+++ encryption_decryption_tool/encryption_decryption_tool/Program.cs
@@ -1,44 +1,63 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
 namespace encryption_decryption_tool
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             //request message
 
             //declare message variable
             string message = "";
+            int key;
+
+            Console.WriteLine("type a messageto encrypt"+Environment.NewLine);
+            message = Console.ReadLine();
+            Console.WriteLine("Enter your key"+Environment.NewLine);
+            key=int.Parse(Console.ReadLine());
+            Console.WriteLine("Encrypted data:");
+            string cipherText=CEncipher(message, key);
+            Console.WriteLine(cipherText+Environment.NewLine);
+            Console.WriteLine("Decrypted data:");
+            Console.WriteLine(CDecipher(cipherText,key)+Environment.NewLine);
+
             //validate message
-            do
-            {
-                Console.WriteLine("Geef de tekst dat u wenst te encrypteren of te decrypteren in: ");
-                Console.ReadLine();
-            } while (string.IsNullOrWhiteSpace(message));
+            //do
+            //{
+            //    Console.WriteLine("Geef de tekst dat u wenst te encrypteren of te decrypteren in: ");
+            //    Console.ReadLine();
+            //} while (string.IsNullOrWhiteSpace(message));
+
+
 
             //functions for encryption
-            static char CCipher(char ch,int key)
+            static char CCipher(char ch, int key)
             {
                 if (!char.IsLetter(ch))
                 {
                     return ch;
                 }
                 char offset = char.IsUpper(ch) ? 'A' : 'a';
-                return (char)((((ch+key)-offset)%26)+offset);
+                return (char)((((ch + key) - offset) % 26) + offset);
 
             }
             //Ceasar cipher encryption
-            static string CEncrypt(string input)
+            static string CEncipher(string input,int key)
             {
-                return "";
+                string output=string.Empty;
+
+                foreach (char ch in input)
+                {
+                    output += CCipher(ch, key);
+                }
+                return output;
             }
 
             //Ceasar cipher deryption
-            static string CDecrypt(string input)
+            static string CDecipher(string input,int key)
             {
-                return "";
+                return CEncipher(input, 26 - key);
             }
         }
     }
