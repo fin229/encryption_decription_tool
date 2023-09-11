24ad68ea597d81949e4aedf86f1ecbb3a96fb119
Fineas <r0845664@student.thomasmore.be>
2023-09-11 02:49:55 +0200

    fucnties aangemaakt om Ceasar cipher te encrypteren en decrypteren. CCypher functie geprogrammeerd.

--- encryption_decryption_tool/encryption_decryption_tool/Program.cs
+++ encryption_decryption_tool/encryption_decryption_tool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace encryption_decryption_tool
 {
@@ -6,7 +7,39 @@ namespace encryption_decryption_tool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //request message
+
+            //declare message variable
+            string message = "";
+            //validate message
+            do
+            {
+                Console.WriteLine("Geef de tekst dat u wenst te encrypteren of te decrypteren in: ");
+                Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(message));
+
+            //functions for encryption
+            static char CCipher(char ch,int key)
+            {
+                if (!char.IsLetter(ch))
+                {
+                    return ch;
+                }
+                char offset = char.IsUpper(ch) ? 'A' : 'a';
+                return (char)((((ch+key)-offset)%26)+offset);
+
+            }
+            //Ceasar cipher encryption
+            static string CEncrypt(string input)
+            {
+                return "";
+            }
+
+            //Ceasar cipher deryption
+            static string CDecrypt(string input)
+            {
+                return "";
+            }
         }
     }
 }
